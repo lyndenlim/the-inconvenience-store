20df1c9731748510b5d01281a36910d1e02552c8
lynden <lyndenlim15@gmail.com>
2022-05-03 17:24:21 -0400

    Display images on homepage

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react"
+import "./Homepage.css"
+import items from "./items"
+
 function HomePage() {
+  const [photos, setPhotos] = useState([])
+
+  useEffect(() => {
+    const photoArray = items.map(item => item.photos)
+    setPhotos(photoArray)
+    // photoArray.map(photo => console.log(photo))
+  }, [])
+
   return (
-    <div>HomePage</div>
+    <div className="item">
+      {photos.map(photo => photo.map(item => <img key={item} className="photo" src={require(`../../photos/${item}.jpeg`)} alt={item} />))}
+    </div>
   )
 }
 
---
+++ src/Components/HomePage/items.js
@@ -0,0 +1,120 @@
+const items = [
+    {
+        id: 1,
+        photos: ["broom1", "broom2", "broom3", "broom4"]
+    },
+    {
+        id: 2,
+        photos: ["buttons"]
+    },
+    {
+        id: 3,
+        photos: ["chain_fork1", "chain_fork2"]
+    },
+    {
+        id: 4,
+        photos: ["chair1", "chair2", "chair3", "chair4"]
+    },
+    {
+        id: 5,
+        photos: ["champagne_glass"]
+    },
+    {
+        id: 6,
+        photos: ["coffee_pot"]
+    },
+    {
+        id: 7,
+        photos: ["cutlery_set1", "cutlery_set2", "cutlery_set3", "cutlery_set4", "cutlery_set5", "cutlery_set6"]
+    },
+    {
+        id: 8,
+        photos: ["door"]
+    },
+    {
+        id: 9,
+        photos: ["glasses"]
+    },
+    {
+        id: 10,
+        photos: ["inflatable_doorknob"]
+    },
+    {
+        id: 11,
+        photos: ["inflatable_paddle"]
+    },
+    {
+        id: 12,
+        photos: ["key1", "key2"]
+    },
+    {
+        id: 13,
+        photos: ["long_mug1", "long_mug2"]
+    },
+    {
+        id: 14,
+        photos: ["mug1", "mug2", "mug3"]
+    },
+    {
+        id: 15,
+        photos: ["mugs1", "mugs2", "mugs3"]
+    },
+    {
+        id: 16,
+        photos: ["pot1", "pot2", "pot3"]
+    },
+    {
+        id: 17,
+        photos: ["rainboots"]
+    },
+    {
+        id: 18,
+        photos: ["ruler"]
+    },
+    {
+        id: 19,
+        photos: ["salt_and_pepper_shakers"]
+    },
+    {
+        id: 20,
+        photos: ["slipper_spoon1", "slipper_spoon2"]
+    },
+    {
+        id: 21,
+        photos: ["staircase"]
+    },
+    {
+        id: 22,
+        photos: ["stairs"]
+    },
+    {
+        id: 23,
+        photos: ["tea_pot1", "tea_pot2"]
+    },
+    {
+        id: 24,
+        photos: ["tea_set"]
+    },
+    {
+        id: 25,
+        photos: ["thick_cutlery_set1", "thick_cutlery_set2", "thick_cutlery_set3", "thick_cutlery_set4", "thick_cutlery_set5", "thick_cutlery_set6"]
+    },
+    {
+        id: 26,
+        photos: ["toothbrush"]
+    },
+    {
+        id: 27,
+        photos: ["umbrella"]
+    },
+    {
+        id: 27,
+        photos: ["watering_can1", "watering_can2", "watering_can3", "watering_can4", "watering_can5"]
+    },
+    {
+        id: 28,
+        photos: ["wine_glass1", "wine_glass2"]
+    },
+]
+
+export default items
