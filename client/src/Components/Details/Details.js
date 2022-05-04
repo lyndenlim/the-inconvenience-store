77619ec2ad5cc3253be44d8d181f183e42251668
lynden <lyndenlim15@gmail.com>
2022-05-04 19:05:32 -0400

    Templating for details page

--- client/src/Components/Details/Details.js
+++ client/src/Components/Details/Details.js
@@ -7,7 +7,9 @@ function Details() {
     const { id } = useParams()
     const [itemDetails, setItemDetails] = useState("")
     const [price, setPrice] = useState("")
-    const [mainPhoto, setMainPhoto] = useState("") 
+    const [quantity, setQuantity] = useState(1)
+    const [mainPhoto, setMainPhoto] = useState("")
+    const [subPhotos, setSubPhotos] = useState([])
 
     useEffect(() => {
         async function getItemDetails() {
@@ -15,18 +17,43 @@ function Details() {
             setItemDetails(data.data)
             setPrice(parseFloat(data.data.price).toFixed(2))
             setMainPhoto(data.data.photos[0])
+            setSubPhotos(data.data.photos.slice(1, data.data.photos.length))
         }
 
         getItemDetails()
-    },[])
+    }, [])
+
+    function addToCart(e) {
+        e.preventDefault()
+        console.log(quantity)
+        setQuantity(1)
+    }
 
     return (
-        <div className="item-detail-container">
-            <p>{itemDetails.name}</p>
-            <p>{`$${price}`}</p>
-            <p>Category: {itemDetails.category}</p>
-            <p>{itemDetails.description}</p>
-            {/* <img src={require(`../../photos/${mainPhoto}.jpeg`)} alt="main"/> */}
+        <div className="item-container">
+            <div className="col-6 item-photo-container" >
+                <img className="main-photo" src={mainPhoto ? require(`../../photos/${mainPhoto}.jpeg`) : null} alt="main" />
+            </div>
+            {subPhotos.length > 0 ?
+                <div className="col-2 sub-photo-container">
+                    {subPhotos.map(photo => <img className="sub-photo" src={require(`../../photos/${photo}.jpeg`)} alt="sub" />)}
+                </div>
+                :
+                null}
+            <div className="col-4 item-detail-container">
+                <h1>{itemDetails.name}</h1>
+                <h3>{`$${price}`}</h3>
+                <p>Category: {itemDetails.category}</p>
+                <p>{itemDetails.description}</p>
+                <form onSubmit={addToCart}>
+                    <input onChange={e => setQuantity(parseInt(e.target.value))} className="quantity" value={quantity} min="0" autoComplete="new-password" type="number" required />
+                    <button onClick={() => setQuantity(quantity => quantity + 1)} type="button">+</button>
+                    <button onClick={() => setQuantity(quantity => quantity - 1)} type="button">-</button>
+                    <br />
+                    <br />
+                    <button type="submit">Add to cart</button>
+                </form>
+            </div>
         </div>
     )
 }
