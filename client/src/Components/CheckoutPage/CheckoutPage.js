f95c1de30909048af2515ab9de29651df5304507
lynden <lyndenlim15@gmail.com>
2022-05-08 18:04:25 -0400

    Reformat checkout page

--- client/src/Components/CheckoutPage/CheckoutPage.js
+++ client/src/Components/CheckoutPage/CheckoutPage.js
@@ -80,7 +80,7 @@ function CheckoutPage({ user, setCartCount, orderNumber, setOrderNumber }) {
                             }, 5000)
                         })
                 } else {
-                    alert("Error!")
+                    alert("Something went wrong.")
                 }
             })
     }
@@ -215,7 +215,58 @@ function CheckoutPage({ user, setCartCount, orderNumber, setOrderNumber }) {
                         </Col>
                         <Col>
                             <Form.Label>State/Province</Form.Label>
-                            <Form.Control onChange={e => setState(e.target.value)} value={state} placeholder="State/Province" required />
+                            <Form.Select onChange={e => setState(e.target.value)} value={state}>
+                                <option value="AL">Alabama</option>
+                                <option value="AK">Alaska</option>
+                                <option value="AZ">Arizona</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="CA">California</option>
+                                <option value="CO">Colorado</option>
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="HI">Hawaii</option>
+                                <option value="ID">Idaho</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IN">Indiana</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NV">Nevada</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="OH">Ohio</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="OR">Oregon</option>
+                                <option value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="TX">Texas</option>
+                                <option value="UT">Utah</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WA">Washington</option>
+                                <option value="WV">West Virginia</option>
+                                <option value="WI">Wisconsin</option>
+                                <option value="WY">Wyoming</option>
+                            </Form.Select>
                         </Col>
                         <Col>
                             <Form.Label>Postcode</Form.Label>
@@ -266,7 +317,14 @@ function CheckoutPage({ user, setCartCount, orderNumber, setOrderNumber }) {
                         return (
                             <div className="checkout-item" key={index}>
                                 <img className="checkout-image" src={require(`../../photos/${item.item.photos[0]}.jpeg`)} alt="item" />
-                                <p>{item.item.name} x{item.quantity} - ${parseFloat(item.total).toFixed(2)}</p>
+                                <div style={{width: "100%", paddingRight: "10px"}}>
+                                    <div className="summary-item-total-container">
+                                        <h4>{item.item.name}</h4>
+                                        <h4>${parseFloat(item.total).toFixed(2)}</h4>
+                                    </div>
+                                    <h5>Qty: {item.quantity}</h5>
+                                </div>
+
                             </div>
                         )
                     }
